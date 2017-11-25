16a8f6c2ce181c44ccb67d01dbc560aca5f5fb4f
Jo <jo.learnstuff@gmail.com>
2017-11-25 16:17:18 +0800

    任務 14：注入 HttpClient 服務元件(fix)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { isNull } from 'util';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
